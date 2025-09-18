050bd415818a85912aa9a996d11abf2e54dcf335
Viktoriya Gromova <rockingirl@mail.ru>
2025-09-18 17:03:35 +0500

    added small corrections to index and style files and rewrote script2.js for practise and understanding. Also added functionality to deleting note cards while the favorite filter is on

---
+++ script2.js
@@ -0,0 +1,246 @@
+const model = {
+    notes: [],
+    addNote(title, description, color) {
+        const newNote = {
+            title: title,
+            id: new Date().getTime(),
+            description: description,
+            color: color,
+            isFavorite: false,
+        }
+        //добавить в начало массива
+        this.notes.unshift(newNote)
+        //обновить отрисовку заметок
+        view.renderNotes(this.notes)
+    },
+    //меняет свойство isFavorite на противоположное
+    toggleFavorite(noteId) {
+        this.notes = this.notes.map(note => {
+            if (note.id === noteId) {
+                note.isFavorite = !note.isFavorite
+            }
+            return note
+        })
+        view.renderNotes(this.notes)
+    },
+    //фильтрует массив, кроме выбранной по id заметки
+    deleteNote(noteId) {
+        this.notes = this.notes.filter(note => {
+            return note.id !== noteId
+        })
+        view.renderNotes(this.notes)
+    },
+    //фильтрует массив с заметками со свойством isFavorite: true
+    filterFavorites() {
+        return this.notes.filter(note => {
+            return note.isFavorite
+        })
+    },
+    countTasks() {
+        return this.notes.length;
+    }
+}
+
+const view = {
+    //основной метод запуска view
+    init() {
+        //элементы формы
+        const form = document.querySelector('.form')
+        const input = document.querySelector('.input')
+        const textarea = document.querySelector('.textarea')
+
+        //элементы выбора цвета заметки
+        const colorUl = document.querySelector('.colors')
+        const radioButtons = document.querySelectorAll('.radio')
+        const firstCircle = document.querySelector('.circle')
+        let selectedColor = firstCircle.classList[1]
+
+        //элементы всплывающих сообщений
+        const alertGreen = document.querySelector('.alert-green')
+        const alertGreenSpan = document.querySelector('.alert-green-span')
+        const alertRed = document.querySelector('.alert-red')
+
+        //контейнер для заметок
+        const notesContainer = document.querySelector('.cards-wrapper')
+        //блок с иконкой избранных заметок
+        const favoritesContainer = document.querySelector('.favorites-span-wrapper')
+
+
+        //обработчик на список с "кнопками" выбора цвета
+        colorUl.addEventListener('click', (event) => {
+            if (event.target.classList.contains('circle')) {
+                selectedColor = event.target.classList[1]
+                radioButtons.forEach(radio => {
+                    radio.classList.remove('selected')
+                })
+                event.target.parentElement.classList.add('selected')
+                return selectedColor
+            }
+        })
+
+        //обработчик на форму
+        form.addEventListener('submit', (event) => {
+            //отменить дефолтную перезагрузку страницы
+            event.preventDefault()
+            //если количество символов заголовка больше 50 => показывать красное сообщение на 3 сек
+            if (input.value.length > 50) {
+                alertRed.classList.add('visible');
+                setTimeout(() => {
+                    alertRed.classList.remove('visible');
+                }, 3000)
+                //передать данные контроллеру, стереть инпуты, показать зеленое сообщение на 3 сек
+            } else {
+                controller.addNote(input.value, textarea.value, selectedColor)
+                input.value = ''
+                textarea.value = ''
+                alertGreen.classList.add('visible');
+                setTimeout(() => {
+                    alertGreen.classList.remove('visible');
+                }, 3000)
+                //обновить отрисовку счетчика заметок
+                view.renderCounter()
+            }
+        })
+
+        //один общий слушатель на родительский элемент заметок, т.е. на контейнер, для отслеживания событий в заметках
+        notesContainer.addEventListener('click', (event) => {
+            //находит айди ближайшей к целевому элементу заметки
+            const noteId = +(event.target.closest('.card').id)
+            if (event.target.closest('.icon-heart')) {
+                controller.toggleFavorite(noteId)
+            }
+            if (event.target.closest('.icon-bucket')) {
+                controller.deleteNote(noteId)
+
+                //показать зеленое сообщение на 3 сек
+                alertGreen.classList.add('visible');
+                alertGreenSpan.textContent = 'Заметка удалена!'
+                setTimeout(() => {
+                    alertGreen.classList.remove('visible');
+                }, 3000)
+                //обновить отрисовку счетчика заметок
+                view.renderCounter()
+            }
+        })
+
+        //общий слушатель на блок с иконкой избранных заметок
+        favoritesContainer.addEventListener('click', (event) => {
+            //переключатель класса, который показывает одну иконку и скрывает другую
+            favoritesContainer.classList.toggle('filter-active');
+            //свойство-переключатель меняет значение на противоположное с каждым кликом
+            view.isFilterActive = !view.isFilterActive
+
+            if (view.isFilterActive) {
+                controller.filterFavorites()
+            } else {
+                view.renderNotes(model.notes)
+            }
+        })
+    },
+
+    //отрисовка заметок
+    renderNotes(notes) {
+        //контейнер для заметок
+        const notesContainer = document.querySelector('.cards-wrapper')
+        //блок с иконкой избранных заметок
+        const favoritesContainer = document.querySelector('.favorites-span-wrapper')
+
+        //если массив с заметками пуст, показывать дефолтное сообщение
+        if (notes.length === 0) {
+            favoritesContainer.style.display = 'none'
+            notesContainer.innerHTML = `
+            <div class="no-cards-message">У вас нет еще ни одной заметки <br> 
+            Заполните поля выше и создайте свою первую заметку!
+            </div>
+            `
+            //отрисовать каждую заметку
+        } else {
+            notesContainer.innerHTML = '';
+            notes.forEach(note => {
+                notesContainer.innerHTML += `
+                <div class="card" id="${note.id}">
+                <div class="card-title-wrapper ${note.color}">
+                    <h2 class="card-title">${note.title}</h2>
+                    <div class="card-icon-wrapper">
+                        <svg class="icon-heart ${note.isFavorite ? 'favorite' : ''}" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.6667 5.90815C14.6667 6.93913 14.2708 7.9294 13.5639 8.66193C11.9366 10.3487 10.3583 12.1075 8.67024 13.7331C8.2833 14.1003 7.66951 14.0869 7.29924 13.7031L2.43588 8.66193C0.965872 7.13813 0.965872 4.67815 2.43588 3.15438C3.92033 1.61563 6.33867 1.61563 7.82311 3.15438L7.9999 3.33761L8.17657 3.15449C8.88831 2.41633 9.85764 2 10.8702 2C11.8828 2 12.8521 2.41629 13.5639 3.15438C14.2709 3.88697 14.6667 4.87718 14.6667 5.90815Z"
+                                      stroke="#3D4D27" stroke-width="1.5" stroke-linejoin="round"/>
+                            </svg>
+                        <svg class="icon-bucket" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.6667 7.33331V13.6C12.6667 13.8209 12.4876 14 12.2667 14H3.73337C3.51246 14 3.33337 13.8209 3.33337 13.6V7.33331"
+                                      stroke="#3D4D27" stroke-width="1.5" stroke-linecap="round"
+                                      stroke-linejoin="round"/>
+                                <path d="M6.66663 11.3333V7.33331" stroke="#3D4D27" stroke-width="1.5"
+                                      stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M9.33337 11.3333V7.33331" stroke="#3D4D27" stroke-width="1.5"
+                                      stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M14 4.66667H10.6667M10.6667 4.66667V2.4C10.6667 2.17909 10.4876 2 10.2667 2H5.73333C5.51242 2 5.33333 2.17909 5.33333 2.4V4.66667M10.6667 4.66667H5.33333M2 4.66667H5.33333"
+                                      stroke="#3D4D27" stroke-width="1.5" stroke-linecap="round"
+                                      stroke-linejoin="round"/>
+                            </svg>
+                    </div>
+                </div>
+                <div class="card-description">${note.description}</div>
+            </div>`
+            })
+            //включить блок с иконкой избранных заметок
+            favoritesContainer.style.display = 'flex'
+
+            //обновить отрисовку счетчика заметок
+            this.renderCounter()
+        }
+    },
+
+    renderCounter() {
+        const counter = document.querySelector('.header-notes-number')
+        counter.textContent = controller.countTasks()
+    },
+
+    //свойство-переключатель для отслеживания работы фильтра избранных заметок и доступа к нему для controller и model
+    isFilterActive: false
+
+}
+
+const controller = {
+    addNote(title, description, color) {
+        //проверка на пустой ввод и ограничение по количеству символов <=50
+        if ((title.trim() !== '') && (description.trim() !== '') && (title.length <= 50)) {
+            model.addNote(title, description, color)
+        }
+    },
+    toggleFavorite(noteId) {
+        model.toggleFavorite(noteId)
+    },
+    deleteNote(noteId) {
+        model.deleteNote(noteId)
+        const favoriteNotes = model.filterFavorites();
+        //проверка на включенный фильтр избранных заметок для перерисовки массива избранных
+        //заметок в случае их удаления при вкл фильтре
+        view.isFilterActive ? view.renderNotes(favoriteNotes) : view.renderNotes(model.notes)
+        //если удалить все избранные заметки из массива избранных заметок, то перерисовываем общий массив карточек
+        // и переключаем фильтр на выкл
+        if (model.filterFavorites().length === 0) {
+            view.isFilterActive = false;
+            view.renderNotes(model.notes)
+        }
+    },
+    filterFavorites() {
+        //сохраняет полученный в модели результат (отфильтрованный массив заметок) в переменную
+        const favoriteNotes = model.filterFavorites();
+        //отрисовывает отфильтрованный моделью массив заметок
+        view.renderNotes(favoriteNotes)
+    },
+    //возвращает полученный в модели результат
+    countTasks() {
+        return model.countTasks()
+    }
+}
+
+function init() {
+    view.init();
+}
+
+//запуск функции init после того, как загрузился контент DOM
+document.addEventListener('DOMContentLoaded', init);
